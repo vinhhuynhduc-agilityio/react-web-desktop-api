4a94ddba49a46dfa8726f57743d7a847f71ed0c9
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-07-11 11:20:07 +0700

    update

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use('/api', router);
+
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the API! Use /api for endpoints.' });
 });
-server.use('/api', router);
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
